74f9dc4f4b81dad40d7a89dcb222174f3c97bb1e
lucasvs07 <78516282+lucasvs07@users.noreply.github.com>
2022-06-15 15:33:04 -0300

    Implementada adição

--- CalculadoraTabajara/Calculo.cs
+++ CalculadoraTabajara/Calculo.cs
@@ -33,5 +33,11 @@ namespace CalculadoraTabajara
             return Num1 + " " + Operador + " " + Num2 + " = " + Resultado;
             ;
         }
+
+        public void Adicao()
+        {
+            Resultado = Num1 + Num2;
+            Operador = "+";
+        }
     }
 }
